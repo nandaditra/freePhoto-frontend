c2b5fcd5db081185885becf91133bec5b71bb436
Nanda Aditya Putra <nandaditra56@gmail.com>
2022-12-07 13:40:44 +0700

    add margin on title mini-title

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -8,8 +8,8 @@ function Main() {
 
             <main className="">
                 <div className="text-center my-3.5">
-                    <h1 className="text-8xl font-bold">freePhoto</h1>
-                    <p className="#">find your photo or images freely</p>
+                    <h1 className="text-8xl font-bold m-6">freePhoto</h1>
+                    <p className="text-3xl text-gray-500 m-6">find your photo or images freely</p>
 
                     <form class="flex items-center w-1/2 m-auto">   
                         <label for="simple-search" class="sr-only">Search</label>
@@ -19,7 +19,7 @@ function Main() {
                             </div>
                             <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
                         </div>
-                        <button type="submit" class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-black dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <button type="submit" class="p-2.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-black dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             
                            Search
                         </button>
