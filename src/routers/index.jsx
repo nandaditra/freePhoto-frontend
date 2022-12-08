6e1370f3117305e8467b94ddebd436997be19c90
Nanda Aditya Putra <nandaditra56@gmail.com>
2022-12-08 12:47:28 +0700

    make login, signin, mainpage and not found router

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Main from "../pages/Main"
+import NotFound from "../pages/NotFound"
+import Signin from "../pages/SignIn"
+import Login from "../pages/Login"
 
 function Routers() {
     return (
         <BrowserRouter>
             <Routes>
                  <Route path="/" element={<Main />} />
+                 <Route path="/p/notfound" element={<NotFound />} />
+                 <Route path="/p/signup" element={<Signin />} />
+                 <Route path="/p/login" element={<Login />} />
             </Routes>
         </BrowserRouter>
     )
