8ebaa930a570947bde30fa95ab378d7e93bcbbd6
Nanda Aditya Putra <nandaditra56@gmail.com>
2022-12-08 12:46:14 +0700

    fix the login and fix the signin

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -9,7 +9,7 @@ function Login() {
            <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
               <div className="col-span-2">
                  <h1 className="text-2xl text-center font-bold m-8">Login</h1>
-                 <div className="block shadow-lg card rounded w-11/12 m-auto text-gray-500 bg-stone-50">
+                 <div className="block shadow-lg card rounded w-10/12 m-auto text-gray-500 bg-stone-50">
                       <form className="">
                           <div className="mt-6 mx-6 pt-6">
                              <label 
@@ -31,7 +31,7 @@ function Login() {
 
                           <div className="grid grid-cols-2 gap-4">
                               <div className="m-auto ml-6">
-                                   Have Account? <b className="text-black">Login</b>
+                                   Don't have account? <b className="text-black">Sign In</b>
                               </div>
                               <div className="text-right">
                                   <button type="submit" className="m-6 px-5 py-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-black dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
