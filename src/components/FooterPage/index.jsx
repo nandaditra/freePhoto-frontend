794d0dbf78f7c24707cf7e3eafb6c19c81c8d00e
Nanda Aditya Putra <nandaditra56@gmail.com>
2022-12-11 16:44:27 +0700

    Make footer Page

---
+++ src/components/FooterPage/index.jsx
@@ -0,0 +1,11 @@
+function FooterPage() {
+    return (
+        <footer className=" p-4 bottom-0 left-0 right-0 bg-stone-300">
+            <div className="text-center">
+                <p>❤ created by nandaditra - 2022</p>
+            </div>
+        </footer>
+    )
+}
+
+export default FooterPage
