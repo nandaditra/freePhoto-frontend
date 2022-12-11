166cd77d1447ef53dbce59c1f6ff534942967efe
Nanda Aditya Putra <nandaditra56@gmail.com>
2022-12-11 16:45:01 +0700

    add search funciton

--- src/components/HeaderPage/index.jsx
+++ src/components/HeaderPage/index.jsx
@@ -1,6 +1,20 @@
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, useNavigate,  } from "react-router-dom"
 
-function HeaderPage() {
+function HeaderPage({link}) {
+    const [query, setQuery ] = useState("")
+    const navigate = useNavigate()
+
+    const HandleSearch = (e) => {
+         e.preventDefault()
+         if(query === "") {
+            console.log("Your keyword still empty")
+            navigate(link)
+         } else {
+            navigate(`/photos/searchphoto/${query}`)
+         }
+    }
+
     return (
         <nav className="bg-white border-gray-200 sm:px-4 py-2.5 rounded sticky top-0">
           <div className="container flex flex-wrap items-center justify-between mx-auto">
@@ -16,13 +30,20 @@ function HeaderPage() {
             <div className="hidden w-100 md:block md:w-auto" id="navbar-default">
     
                 <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
-                    <form className="flex items-center w-1/2 m-auto">   
+                    <form className="flex items-center w-1/2 m-auto"
+                     onSubmit={HandleSearch}>   
                                 <label htmlFor="simple-search" className="sr-only">Search</label>
                                     <div className="relative w-full">
                                         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                                             <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
                                         </div>
-                                        <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
+                                        <input 
+                                          type="text" 
+                                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                          value={query}
+                                          onChange={(e) => setQuery(e.target.value)}
+                                          placeholder="Search" 
+                                          required />
                                     </div>
                                 <button type="submit" className="p-2.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-black dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                         
