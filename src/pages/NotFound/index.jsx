1c229c388d0993707bed8fd451f13af0e8fbca56
Nanda Aditya Putra <nandaditra56@gmail.com>
2022-12-11 16:34:26 +0700

    Make logic navigate, query, and handleSearch

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,7 +1,22 @@
 import Header from "../../components/Header"
 import Footer from "../../components/Footer"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 function NotFound() {
+    const [query, setQuery] = useState("")
+    const navigate = useNavigate()
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        if(query === ""){
+           console.log("query is still empty")
+           navigate('/notfound')
+        } else {
+           navigate(`/photos/searchphoto/${query}`)
+        }      
+    }
+
     return (              
          <>
             <Header />
@@ -12,15 +27,27 @@ function NotFound() {
                     <p className="text-3xl text-gray-500 m-6">we are sorry,but the keyword <br /> requested was not found</p>
                     <p className="text-2xl text-gray-500 m-8">Keep on, try again!</p>
 
-                    <form class="flex items-center w-1/2 m-auto">   
-                        <label for="simple-search" class="sr-only">Search</label>
-                        <div class="relative w-full">
-                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                    <form 
+                       className="flex items-center w-1/2 m-auto"
+                       onSubmit={handleSearch}
+                        >   
+                        <label htmlFor="simple-search" className="sr-only">Search</label>
+                        <div className="relative w-full">
+                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
                             </div>
-                            <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
+                            <input 
+                               type="text" 
+                               id="simple-search" 
+                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                               value={query}
+                               onChange ={(e) => setQuery(e.target.value)}
+                               placeholder="Search" 
+                               required />
                         </div>
-                        <button type="submit" class="p-2.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-black dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <button 
+                           type="submit" 
+                           className="p-2.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-black dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             
                            Search
                         </button>
--- src/pages/OverviewPhoto/index.jsx
+++ src/pages/OverviewPhoto/index.jsx
@@ -1,15 +1,148 @@
+import { useEffect, useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import api from "../../api"
 import Footer from "../../components/Footer"
+import FooterPage from "../../components/FooterPage"
 import HeaderPage from "../../components/HeaderPage"
+import Loading from "../Loading"
 
 function OverviewPhoto() {
-    return (
-        <>
-          <HeaderPage/>
+    const [ data, setData ] = useState([])
+    const [ isLoading, setLoading] = useState(false)
+    const { photoId } = useParams() 
+    const navigate = useNavigate()
+    const photos = data.find((photo)=> photo.id === photoId) 
 
+    const fetchData = async() => {
+        try {
+          setLoading(true)
+          const response = await api.get()
+          setData(response.data)  
+        } catch (error) {
+          console.log(error)
+          setLoading(false)
+        }
+    }
 
-          <Footer/>
-        </>
-    )
+    useEffect(() => {
+        setTimeout(()=>{
+            if(isLoading === false) {
+               console.log("Loading ...")
+            }
+            fetchData()
+         }, [2000])
+    }, [isLoading])
+
+    console.log(photos) 
+
+    if(photos === false) {
+       navigate('/notfound')
+       console.log("Data is Not Found")
+    } 
+    
+    if(isLoading === false) {
+      return (
+         <>
+           <HeaderPage />
+           <Loading />
+           <Footer/>
+         </>
+      )
+    } else
+       {
+        return (
+            <>
+              <HeaderPage/>
+
+              <main className="">
+                  <div className="m-6 grid grid-cols-10 gap-4">
+                     <div className="col-span-7">
+                        <img
+                          className="w-full rounded-md" 
+                          src={photos?.urls.raw} 
+                          alt={photos?.alt_description}
+                          />
+                     </div>
+                     <div className=" m-2 col-span-3">
+                          <div className="m-auto p-2">
+                              <img
+                                  className="w-12 rounded-full float-left mr-3" 
+                                  src={photos?.user.profile_image.medium} 
+                                  alt={photos?.alt_description}
+                              />
+                              <h6 className="p-2 font-bold"> {photos?.user.username}</h6>   
+                          </div>    
+
+                          <div className="p-2">
+                              <p> {photos?.user.bio}</p>
+                              <div className="grid grid-cols-3 gap-4 mt-5 text-center text-white">
+                                  <div className="cols-span-1 px-5 py-2.5 bg-green-600 rounded-full">
+                                      🎪 {photos?.user.total_collections}
+                                  </div>
+                                  <div className="cols-span-1 px-5 py-2.5 bg-rose-600 rounded-full">
+                                      ❤ {photos?.user.total_likes}
+                                  </div>
+                                  <div className="cols-span-1 px-5 py-2.5 bg-blue-600 rounded-full">
+                                      🖼 {photos?.user.total_photos}
+                                  </div>
+                              </div>  
+                          </div>
+
+                          <div className="bg-stone-50 my-6">
+                            <h2 className="text-center">Image Description</h2>
+                            <table className="p-3 m-3 m-auto">
+                                <tbody className="m-4">
+                                    <tr className="">
+                                       <td className="pr-40">
+                                          Image Type 
+                                       </td>
+                                       <td className="text-right">
+                                          JPEG
+                                       </td>
+                                    </tr>
+                                    <tr>
+                                       <td className="pr-40">
+                                          Resolution 
+                                       </td>
+                                       <td className="text-right">
+                                          {photos?.width}px X {photos?.height}px
+                                       </td>
+                                    </tr>
+                                    <tr className="">
+                                       <td className="pr-40">
+                                          Location
+                                       </td>
+                                       <td className="text-right">
+                                          {photos?.user.location}
+                                       </td>
+                                    </tr>
+                                    <tr>
+                                       <td className="pr-40">
+                                          Fotografer
+                                       </td>
+                                       <td className="floar-right text-right">
+                                          {photos?.user.name}
+                                       </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                          </div>
+
+                          <button 
+                             type="submit"
+                             className="m-2 py-3 w-full text-white bg-green-600 rounded-full">
+                             Download  
+                          </button>
+
+                          <h2 className="text-center text-2xl font-bold mt-6">Related Photo</h2>
+                                                           
+                     </div>
+                  </div>     
+              </main>
+              <FooterPage/>
+            </>
+        )
+    }
 }
 
 export default OverviewPhoto
