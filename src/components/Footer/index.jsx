481bf15c73269a5673ad9e205350681bdbf84dac
Nanda Aditya Putra <nandaditra56@gmail.com>
2022-12-11 16:43:42 +0700

    Fix the bugs

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer className=" p-4 bottom-0 left-0 right-0 bg-stone-300">
+        <footer className=" p-4 absolute bottom-0 left-0 right-0 bg-stone-300">
             <div className="text-center">
                 <p>❤ created by nandaditra - 2022</p>
             </div>
