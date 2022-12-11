959646a50d479e2e7b731c41f97fc94d57d73618
Nanda Aditya Putra <nandaditra56@gmail.com>
2022-12-11 16:40:17 +0700

    make loading processing

---
+++ src/pages/Loading/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from '../../components/Footer'
+
+const Loading = () => {
+    return (
+        <div>
+            <h1 className="text-center text-2xl">Loading . . .</h1>
+            <Footer />
+        </div>
+    )
+}
+
+export default Loading
