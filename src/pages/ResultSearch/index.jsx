097d787c6f907706e6acf7fc0e00401ed8eeff5c
Nanda Aditya Putra <nandaditra56@gmail.com>
2022-12-12 11:47:18 +0700

    fix the this components

--- src/pages/ResultSearch/index.jsx
+++ src/pages/ResultSearch/index.jsx
@@ -1,35 +1,75 @@
+import { useEffect, useState } from "react"
+import { Link, useParams } from "react-router-dom"
+import storelink from "../../redux/storelink.js"
+import api from "../../api/api.js"
 import Footer from "../../components/Footer"
+import FooterPage from "../../components/FooterPage"
 import HeaderPage from "../../components/HeaderPage"
+import Loading from "../Loading"
 
 function ResultSearch() {
-    return (
+    const [data, setData] = useState([])
+    const [isLoading, setLoading ] = useState(false)
+    const { queryId } = useParams()
+    console.log(queryId)
+
+    const fetchData = async() => {
+        try {
+            setLoading(true)
+            const response = await api.get('search/photos?query='+queryId)
+            setData(response.data)  
+          } catch (error) {
+            console.log(error)
+            console.error()
+            setLoading(false)
+          }
+    } 
+
+    useEffect(()=> {
+        setTimeout(()=>{
+            if(isLoading === false) {
+                console.log("loading...")
+            }
+            const linkdata = storelink
+            linkdata.link = '/search/photos?query='+queryId 
+            console.log(linkdata.link)
+            fetchData()
+        }, [1500])       
+    })
+
+    if(isLoading === false) {
+        return (
+            <>
+               <HeaderPage />
+               <Loading/>
+               <Footer />
+            </>
+        )
+    } else {
+        return (
         <>
          <HeaderPage />
 
-         <div className="m-5">
-             <h1 className="text-2xl font-bold">Result Keyword : </h1>
-             <p>Total : </p>
+         <div className="m-8">
+             <h1 className="text-2xl font-bold">Result Keyword : {queryId}</h1>
+             <p>Total : {data.total}</p>
 
              <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
-                <div>
-                    Gambar 1
-                </div>
-                <div>
-                    Gambar 2
-                </div>
-                <div>
-                    Gambar 3
-                </div>
-                <div>
-                    Gambar 4
-                </div>
+                {data.results?.map((photo, index)=> 
+                    <Link to={`/photos/search/${photo.id}`} key={photo?.id}>
+                        <div className="cols-span-1" key={index}>
+                            <img src={photo.urls.raw} alt={photo.alt_description}/>
+                        </div>
+                    </Link>
+                )}
              </div>
 
          </div>
 
-         <Footer/>
+         <FooterPage />
         </>
     )
+    }
 }
 
 export default ResultSearch
