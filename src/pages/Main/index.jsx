1074a705db11edfbbf57200601e23aee628b56ec
Nanda Aditya Putra <nandaditra56@gmail.com>
2022-12-13 18:21:34 +0700

    Stylsish and media query

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -7,6 +7,7 @@ import { onAuthStateChanged } from "firebase/auth"
 import { auth } from "../../authentication/firebase"
 import Loading from "../Loading"
 import FooterPage from "../../components/FooterPage"
+import "./main.scss"
 
 function Main() {
     const [ data, setData ] = useState([])
@@ -63,7 +64,7 @@ function Main() {
             }
         })
     }, [])
-    
+
     if(isLoading === false && error === null) {
         return (
             <>
@@ -83,7 +84,7 @@ function Main() {
                                     <input 
                                        type="text" 
                                        id="simple-search" 
-                                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-black focus:border-black block w-full pl-10 p-2.5  dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-black dark:focus:border-black" 
                                        placeholder="Search" 
                                        name="query"
                                        value={query}
@@ -92,7 +93,7 @@ function Main() {
                                 </div>
                                 <button 
                                     type="submit" 
-                                    className="p-2.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-black dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                    className="p-2.5 px-3 ml-2 text-sm font-medium text-white bg-black rounded-lg border border-black hover:bg-black focus:ring-4 focus:outline-none focus:ring-black-300 dark:bg-black dark:hover:bg-black dark:focus:ring-black"
                                 > 
                                 Search
                                 </button>
@@ -122,7 +123,7 @@ function Main() {
                                     <input 
                                        type="text" 
                                        id="simple-search" 
-                                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-black focus:border-black block w-full pl-10 p-2.5  dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-black dark:focus:border-black" 
                                        placeholder="Search" 
                                        value={query}
                                        onChange={(e) => setQuery(e.target.value)}
@@ -130,7 +131,7 @@ function Main() {
                                 </div>
                                 <button 
                                     type="submit" 
-                                    className="p-2.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-black dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                    className="p-2.5 px-3 ml-2 text-sm font-medium text-white bg-black rounded-lg border border-black hover:bg-black focus:ring-4 focus:outline-none focus:ring-black-300 dark:bg-black dark:hover:bg-black dark:focus:ring-black"
                                 > 
                                 Search
                                 </button>
